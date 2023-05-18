60d9fcd9fbd7e25b9e1de75474b7c1c3b414b5d0
cody <qudgus1984@naver.com>
2023-05-18 10:31:05 +0900

    [Setting] Project gitignore 처리 및 초기 세팅

---
+++ SM_SoftwearEnginnering_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SM_SoftwearEnginnering_iOS
+//
+//  Created by 이병현 on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
