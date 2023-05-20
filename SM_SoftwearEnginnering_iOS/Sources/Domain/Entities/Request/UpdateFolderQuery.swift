aab7576e05f870e011f5038e8e46e8b6923f8c57
cody <qudgus1984@naver.com>
2023-05-20 16:50:03 +0900

    [Add] Model 구성 완료

---
+++ SM_SoftwearEnginnering_iOS/Sources/Domain/Entities/Request/UpdateFolderQuery.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateFolderQuery.swift
+//  SM_SoftwearEnginnering_iOS
+//
+//  Created by 이병현 on 2023/05/20.
+//
+
+import Foundation
---
+++ SM_SoftwearEnginnering_iOS/Sources/Presents/Main/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  SM_SoftwearEnginnering_iOS
+//
+//  Created by 이병현 on 2023/05/20.
+//
+
+import Foundation
