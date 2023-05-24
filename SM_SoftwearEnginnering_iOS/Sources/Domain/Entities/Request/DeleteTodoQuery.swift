26dc8fcf918ccb9319e8099c04c4d4d704a1624d
cody <qudgus1984@naver.com>
2023-05-24 16:21:50 +0900

    [Add] DeleteTodoUseCase 구성 완료

---
+++ SM_SoftwearEnginnering_iOS/Sources/Domain/Entities/Request/DeleteTodoQuery.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteTodoQuery.swift
+//  SM_SoftwearEnginnering_iOS
+//
+//  Created by 이병현 on 2023/05/24.
+//
+
+import Foundation
+
+struct DeleteTodoQuery: Hashable {
+    let todoId: Int64
+}
